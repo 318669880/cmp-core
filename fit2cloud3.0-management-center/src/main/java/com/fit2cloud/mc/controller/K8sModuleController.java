561c114c7f0e3cfb745484ef7825dd1bf07c55a7
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2020-11-16 12:31:32 +0800

    fix: 重载节点样式调整

--- fit2cloud3.0-management-center/src/main/java/com/fit2cloud/mc/controller/K8sModuleController.java
+++ fit2cloud3.0-management-center/src/main/java/com/fit2cloud/mc/controller/K8sModuleController.java
@@ -1,24 +1,13 @@
 package com.fit2cloud.mc.controller;
 
-import com.fit2cloud.mc.common.constants.ModuleStatusConstants;
 import com.fit2cloud.mc.dto.request.OperatorModuleRequest;
-import com.fit2cloud.mc.model.ModelNode;
 import com.fit2cloud.mc.service.K8sOperatorModuleService;
 import com.fit2cloud.mc.service.ModelManagerService;
-import com.fit2cloud.mc.service.ModuleNodeService;
-import com.fit2cloud.mc.strategy.service.NodeOperateService;
-import org.apache.commons.collections4.CollectionUtils;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
-
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
+
 
 @RestController
 @RequestMapping("/k8s-operator-module")

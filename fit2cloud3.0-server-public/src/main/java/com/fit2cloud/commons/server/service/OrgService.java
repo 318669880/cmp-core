ec6e454494bf097a7090bfad88d2ca01ecc477f1
junjie <junjie.xia@fit2cloud.com>
2020-11-25 13:28:38 +0800

    feat: 多级org获取树状结构数据

--- fit2cloud3.0-server-public/src/main/java/com/fit2cloud/commons/server/service/OrgService.java
+++ fit2cloud3.0-server-public/src/main/java/com/fit2cloud/commons/server/service/OrgService.java
@@ -1,10 +1,14 @@
 package com.fit2cloud.commons.server.service;
 
 import com.fit2cloud.commons.server.base.domain.Organization;
+import com.fit2cloud.commons.server.base.domain.OrganizationExample;
 import com.fit2cloud.commons.server.base.mapper.OrganizationMapper;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Author gin
@@ -17,5 +21,25 @@ public class OrgService {
 
     public Organization getOrg(String id) {
         return organizationMapper.selectByPrimaryKey(id);
+    }
+
+    public List<Organization> getOrgTreeByFirstId(String id) {
+        List<Organization> organizationList = new ArrayList<>();
+        Organization organization = organizationMapper.selectByPrimaryKey(id);
+        organizationList.add(organization);
+        getTree(organizationList, organization);
+        return organizationList;
+    }
+
+    private void getTree(List<Organization> organizationList, Organization organization) {
+        OrganizationExample organizationExample = new OrganizationExample();
+        organizationExample.createCriteria().andPidEqualTo(organization.getId());
+        List<Organization> organizationList1 = organizationMapper.selectByExample(organizationExample);
+        if (CollectionUtils.isNotEmpty(organizationList1)) {
+            organizationList.addAll(organizationList1);
+            for (Organization org : organizationList1) {
+                getTree(organizationList, org);
+            }
+        }
     }
 }

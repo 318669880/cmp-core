c569865e0272589a7f93b0634c1ef56e4b209da2
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2020-12-15 11:16:53 +0800

    feat: cmp集群配置从bootstrap.properties移植到fit2cloud.properties

--- fit2cloud3.0-management-center/src/main/java/com/fit2cloud/mc/Application.java
+++ fit2cloud3.0-management-center/src/main/java/com/fit2cloud/mc/Application.java
@@ -1,5 +1,6 @@
 package com.fit2cloud.mc;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.quartz.QuartzAutoConfiguration;
@@ -8,6 +9,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.*;
 
 
 @SpringBootApplication(exclude = QuartzAutoConfiguration.class)
@@ -18,8 +22,35 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAsync
 public class Application {
 
+    private final static String path = "/opt/fit2cloud/conf/fit2cloud.properties";
+
+    private final static String[] env_keys = {
+            "spring.cloud.config.uri",
+            "spring.cloud.config.profile",
+            "spring.cloud.config.label",
+            "spring.cloud.config.name",
+            "management.endpoints.web.exposure.include"
+    };
+
     public static void main(String[] args) {
-        System.setProperty("spring.config.additional-location", "/opt/fit2cloud/conf/");
-        SpringApplication.run(Application.class, args);
+        try {
+            Properties properties = readFile();
+            Arrays.stream(env_keys).forEach(key -> setEnv(key, properties));
+            SpringApplication.run(Application.class, args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Properties readFile() throws Exception{
+        Properties properties = new Properties();
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
+        properties.load(bufferedReader);
+        return properties;
+    }
+
+    private static void setEnv(String key, Properties properties){
+        if (properties.containsKey(key))
+        System.setProperty(key, properties.getProperty(key));
     }
 }

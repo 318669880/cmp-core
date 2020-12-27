68496fee1fbea3a6eb6113dab291771bb15aafdc
fit2cloud-chenyw <yawen.chen@fit2cloud.com>
2020-12-27 11:49:53 +0800

    fix: 新增组织机构跟节点函数

--- fit2cloud3.0-management-center/src/main/resources/db/migration/V5__init_tables_three.sql
+++ fit2cloud3.0-management-center/src/main/resources/db/migration/V5__init_tables_three.sql
@@ -66,14 +66,3 @@ CREATE TABLE IF NOT EXISTS `config_properties` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE DEFINER=`root`@`%` FUNCTION `GET_ROOT_ORG_ID`(org_id VARCHAR(200)) RETURNS varchar(200) CHARSET utf8mb4
-BEGIN
-	DECLARE rootid VARCHAR(200);
-	DECLARE sid VARCHAR(200);
-	SET sid = org_id;
-		WHILE sid IS NOT NULL DO
-			SET rootid = sid;
-			SELECT pid INTO sid from organization where id = sid;
-		END WHILE;
-	return rootid;
-END
---
+++ fit2cloud3.0-management-center/src/main/resources/db/migration/v7__function.sql
@@ -0,0 +1,11 @@
+CREATE IF NOT EXISTS DEFINER=`root`@`%` FUNCTION `GET_ROOT_ORG_ID`(org_id VARCHAR(200)) RETURNS varchar(200) CHARSET utf8mb4
+BEGIN
+	DECLARE rootid VARCHAR(200);
+	DECLARE sid VARCHAR(200);
+	SET sid = org_id;
+		WHILE sid IS NOT NULL DO
+			SET rootid = sid;
+			SELECT pid INTO sid from organization where id = sid;
+		END WHILE;
+	return rootid;
+END
